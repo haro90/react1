8b04af948661e1ef2ad4bfcb3e8f951878f356c6
Carlos lap <haro.arq@gmail.com>
2023-04-02 18:58:13 -0600

    css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ItemCount from './components/ItemCount/ItemCount';
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar className='NavBar'/>
   <ItemListContainer greeting={"Bienvenidos"}/>
   <ItemCount initial={1} stock={10} onAdd={(quantity)=> console.log('Cantidad agregada', quantity)}/>
     </div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,8 +2,8 @@ import CartWidget from '../CartWidget/CartWidget.js'
 import Logo from './assets/logo.png'
 const NavBar = () =>{
     return(
-        <nav>
-          <img src={Logo} alt="Logo"/>
+        <nav className='nav'>
+          <img src={Logo} alt="Logo" className='logo'/>
            <h3>Tienda oficial Biobest</h3>
            <div>
             <button>Control Biologico</button>
