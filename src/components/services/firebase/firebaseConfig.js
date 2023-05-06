8606d33413c24dddc82acd11a04351b86d19fdbd
Carlos lap <haro.arq@gmail.com>
2023-05-05 18:45:26 -0600

    final 3

--- src/components/services/firebase/firebaseConfig.js
+++ src/components/services/firebase/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getFirestore, collection } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC_Ks0GVP7q4gDqbGTUxl5u2Vaugaybs9s",
-  authDomain: "react-1358e.firebaseapp.com",
-  projectId: "react-1358e",
-  storageBucket: "react-1358e.appspot.com",
-  messagingSenderId: "567213134539",
-  appId: "1:567213134539:web:55745748170102225c414e",
-  measurementId: "G-P6WTE4T1M0"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 // Initialize Firebase
